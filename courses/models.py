b5d17e51ca908efa84f1ebb770d411275d5c7b9f
Olesja1509 <olesja.janjic@gmail.com>
2023-10-25 14:12:18 +0200

    HomeworkDRF

---
+++ courses/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    preview = models.ImageField(upload_to='courses/', verbose_name='Изображение', **NULLABLE)
+    body = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+    body = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='lessons/', verbose_name='Изображение', **NULLABLE)
+    video_url = models.URLField(max_length=200, verbose_name='Ссылка на видео')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
