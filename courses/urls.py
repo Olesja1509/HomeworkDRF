1fc9d8db7144cd158ef44b56624dd19d371ed860
Olesja1509 <olesja.janjic@gmail.com>
2023-10-25 14:31:55 +0200

    HomeworkDRF

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,21 @@
+from django.urls import path
+
+from courses.apps import CoursesConfig
 from rest_framework.routers import DefaultRouter
 
-from users.apps import UsersConfig
-from users.views import UserViewSet
+from courses.views.course import CourseViewSet
+from courses.views.lesson import LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, \
+    LessonDestroyAPIView
 
-app_name = UsersConfig.name
+app_name = CoursesConfig.name
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
+router.register(r'courses', CourseViewSet, basename='courses')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='create_lesson'),
+    path('lesson/', LessonListAPIView.as_view(), name='list_lesson'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='get_lesson'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='update_lesson'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='delete_lesson'),
+] + router.urls
--- users/urls.py
+++ users/urls.py
@@ -1,20 +1,11 @@
-from django.urls import path
-
-from courses.apps import CoursesConfig
 from rest_framework.routers import DefaultRouter
 
-from courses.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView
+from users.apps import UsersConfig
+from users.views import UserViewSet
 
-app_name = CoursesConfig.name
+app_name = UsersConfig.name
 
 router = DefaultRouter()
-router.register(r'courses', CourseViewSet, basename='courses')
+router.register(r'users', UserViewSet, basename='users')
 
-urlpatterns = [
-    path('lesson/create/', LessonCreateAPIView.as_view(), name='create_lesson'),
-    path('lesson/', LessonListAPIView.as_view(), name='list_lesson'),
-    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='get_lesson'),
-    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='update_lesson'),
-    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='delete_lesson'),
-] + router.urls
+urlpatterns = router.urls
